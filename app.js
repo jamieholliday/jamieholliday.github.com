b088be11342aadd055c0bb9f380acb715d5ddfef
Jamie Holliday <jamie@jamieholliday.co.uk>
2014-05-10 16:54:36 +0100

    Added small express server

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
-var express = require('express'),
-	passport = require('passport'),
-	LocalStrategy = require('passport-local').Strategy,
-	flash = require('connect-flash'),
-	util = require('util'),
-	mongojs = require('mongojs');
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
 
 var app = express();
 
-app.configure(function(){
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.cookieParser());
-	app.use(app.router);
-	app.use(express.errorHandler({dumpExceptions:true, showStack:true}));
-});
+app.use(favicon('public/favicon.ico'));
+app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(4711, function(){
-	console.log('Express is running');
-})
+app.use('/', routes);
+
+/// catch 404 and forwarding to error handler
+//app.use(routes);
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+
+// /// error handlers
+
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
+
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
+
+
+module.exports = app;
