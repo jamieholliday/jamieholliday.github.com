329a8de68e6966cfc60184236e70d8d40053babf
Jamie Holliday <jamie.holliday@vaimo.com>
2014-08-05 07:42:57 +0100

    Basic styling for homepage

--- app/modules/projects/projects.js
+++ app/modules/projects/projects.js
@@ -12,6 +12,21 @@ angular.module('jhApp')
 				name: 'project2',
 				description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores explicabo, velit tempora molestiae, ullam provident magnam pariatur nam accusantium necessitatibus quis praesentium veniam, minima mollitia tempore beatae laborum eligendi autem!',
 				tags: 'HTML5, JS'
+			},
+			{
+				name: 'project3',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores explicabo, velit tempora molestiae, ullam provident magnam pariatur nam accusantium necessitatibus quis praesentium veniam, minima mollitia tempore beatae laborum eligendi autem!',
+				tags: 'HTML5, JS'
+			},
+			{
+				name: 'project4',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores explicabo, velit tempora molestiae, ullam provident magnam pariatur nam accusantium necessitatibus quis praesentium veniam, minima mollitia tempore beatae laborum eligendi autem!',
+				tags: 'HTML5, JS'
+			},
+			{
+				name: 'project5',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores explicabo, velit tempora molestiae, ullam provident magnam pariatur nam accusantium necessitatibus quis praesentium veniam, minima mollitia tempore beatae laborum eligendi autem!',
+				tags: 'HTML5, JS'
 			}
 		]
 	};
