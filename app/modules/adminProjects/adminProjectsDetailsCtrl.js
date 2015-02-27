1f5b64447cd93065d2c4fbcadb86609ad9cf94ea
Jamie Holliday <jamie@jamieholliday.com>
2015-02-27 07:27:37 +0000

    Added test for adminProjectDetailsCtrl

--- app/modules/adminProjects/adminProjectsDetailsCtrl.js
+++ app/modules/adminProjects/adminProjectsDetailsCtrl.js
@@ -2,63 +2,63 @@
 angular.module('jhApp')
 .controller('adminProjectsDetailsCtrl', function($stateParams, $location, resourceCache, jhNotifier) {
 
-	var _updatePost,
-		_newPost,
-		project,
-        adminProjectsDetails = this;
+	var adminProjectsDetails = this;
 
 	//setup
-	project = {
+	adminProjectsDetails.project = {
 		published: false
 	};
 
 	//if there is an id this edit if not its new
 	if($stateParams && $stateParams.id) {
 		resourceCache.get('project', {id:$stateParams.id}).then(function(obj) {
-			adminProjectsDetails.project = project = obj;
+			adminProjectsDetails.project = obj;
 		});
-	} else {
-		adminProjectsDetails.project = project;
 	}
 
 	//Public
 	adminProjectsDetails.publish = function(bool) {
-		project.published = bool;
+		adminProjectsDetails.project.published = bool;
 	};
 
 	adminProjectsDetails.saveForm = function(isValid) {
-		if (!isValid) return;
+		if (!isValid) { 
+			return false; 
+		}
 		if(adminProjectsDetails.project._id) {
-			_updatePost();
+			adminProjectsDetails._updateProject();
 		} else {
-			_newPost();
+			adminProjectsDetails._newProject();
 		}
 	};
 
 	adminProjectsDetails.delete = function() {
-		resourceCache.delete('project', {id:project._id}).then(function(responceData) {
+		resourceCache.delete('project', {id:adminProjectsDetails.project._id})
+		.then(function(responceData) {
 			if(responceData.deleted === true) {
 				$location.path('/adminprojects');
 			}
 		});
-	}
+	};
 
 	//Private
-	_updatePost = function() {
-		resourceCache.update('project', {id:project._id}, project).then(function(responceData) {
+	adminProjectsDetails._updateProject = function() {
+		resourceCache.update('project', {id:adminProjectsDetails.project._id}, adminProjectsDetails.project)
+		.then(function(responceData) {
 			if(responceData.message === 'Success') {
 				jhNotifier.notify('Updated project');
 			}
 		});
-	}
+	};
 
-	_newPost = function() {
+	adminProjectsDetails._newProject = function() {
 		//TODO use promise here
-		resourceCache.save('project', project).then(function(responceData) {
+		resourceCache.save('project', adminProjectsDetails.project)
+		.then(function(responceData) {
 			if(responceData._id) {
 				$location.path('/adminprojects/' + responceData._id);
 				jhNotifier.notify('Saved project');
 			}
 		});
-	}
+	};
 });
