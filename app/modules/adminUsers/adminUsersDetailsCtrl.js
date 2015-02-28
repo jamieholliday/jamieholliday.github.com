9f22a68b5ed8741d4eb5854382b1080abd6ba446
Jamie Holliday <jamie@jamieholliday.com>
2015-02-28 17:36:35 +0000

    added test for adminUsersDetailsCtrl

--- app/modules/adminUsers/adminUsersDetailsCtrl.js
+++ app/modules/adminUsers/adminUsersDetailsCtrl.js
@@ -1,25 +1,27 @@
 'use strict';
 angular.module('jhApp')
-.controller('adminUsersDetailsCtrl', function($stateParams, $location, users, jhNotifier) {
+.controller('adminUsersDetailsCtrl', function($stateParams, users, jhNotifier) {
 
-	var _updateUser,
-        adminUsersDetails = this;
+	var adminUsersDetails = this;
 
-	users.get({id:$stateParams.id}).$promise.then(function(obj) {
+	users.get({id:$stateParams.id}).$promise
+	.then(function(obj) {
 		adminUsersDetails.user = obj;
 	});
 
 	adminUsersDetails.saveForm = function(isValid) {
-		if (!isValid) return;
-		_updateUser();
+		if (!isValid) { 
+			return false;
+		}
+		adminUsersDetails._updateUser();
 	};
 
-	_updateUser = function() {
+	adminUsersDetails._updateUser = function() {
 		users.update({id:adminUsersDetails.user._id}, adminUsersDetails.user).$promise.then(function(responceData) {
 			if(responceData.message === 'Success') {
 				jhNotifier.notify('Updated user');
 			}
 		});
-	}
+	};
 
 });
