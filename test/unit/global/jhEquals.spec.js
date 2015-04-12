039ce8442dff0d9d465d0852b748a4ed496620f2
Jamie Holliday <jamie@jamieholliday.com>
2015-04-12 16:23:30 +0100

    Trying to figure out how to test jhEquals directive

--- test/unit/global/jhEquals.spec.js
+++ test/unit/global/jhEquals.spec.js
@@ -1,26 +1,36 @@
-describe('adminBurndown', function() {
+describe('jhEquals', function() {
 	'use strict';
 
 	var scope,
-		elem;
+		dirElement;
 
 		beforeEach(function() {
 			module('jhApp');
+			module('templates');
 
 			inject(function($rootScope, $compile) {
 		        scope = $rootScope;
+		        scope.user = {};
 
-		        elem = angular.element('<input type="password" name="password" ng-model="adminUsersDetails.user.password" jh-equals="{{adminUsersDetails.user.passwordconfirm}}"><input type="password" name="newpasswordconfirm" ng-model="adminUsersDetails.user.passwordconfirm">');
+		        dirElement = angular.element('<input id="password" type="password" name="password" ng-model="user.password" jh-equals="{{user.passwordconfirm}}"><input id="newpasswordconfirm" type="password" name="newpasswordconfirm" ng-model="user.passwordconfirm" jh-equals="{{user.password}}>');
 
-		        $compile(elem)(scope);
-		        scope.$digest();
+		        $compile(dirElement)(scope);
 			});
 
+			scope.$digest();
 		});
 
 
-	// it('should check if values are equal', function() {
-		
-	// });
+	it('should check if values are equal', function() {
+		var passwordInput = dirElement.find('#password');
+		var confirmPasswordInput = dirElement.find('#newpasswordconfirm');
+
+		angular.element(passwordInput).val('testpassword').trigger('input');
+		angular.element(confirmPasswordInput).val('nottestpassword').trigger('input');
+		scope.$digest();
+
+		//expect this to be invalid
+
+	});
 
 });
